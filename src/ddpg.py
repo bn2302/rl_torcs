fixed some bugs regarding reward calculation, added batch normalization

src/ddpg.py
@@ -1,7 +1,6 @@
 import docker
 
-from networks import ActorNetwork
-from networks import CriticNetwork
+from networks import ActorNetwork, CriticNetwork
 from replay_buffer import ReplayBuffer
 from gym_torcs_docker import TorcsDockerEnv
 from numpy.random import seed, randn
@@ -15,6 +14,9 @@ def ou_func(x, mu, theta, sigma):
 
 
 def play_game(train_indicator=1):
+
+    checkpoint_file = '../weights/model.ckpt'
+    logdir = '../logs/train'
     # 1 means Train, 0 means simply Run
     buffer_size = 100000
     batch_size = 32
@@ -30,7 +32,7 @@ def play_game(train_indicator=1):
 
     explore = 100000.
     episode_count = 2000
-    max_steps = 100000
+    max_steps = 10000
     done = False
     step = 0
     epsilon = 1
@@ -44,6 +46,27 @@ def play_game(train_indicator=1):
     critic = CriticNetwork(sess, state_dim, action_dim, tau, lrc)
     buff = ReplayBuffer(buffer_size)  # Create replay buffer
     saver = tf.train.Saver()
+    train_writer = tf.summary.FileWriter(logdir, sess.graph)
+    with tf.name_scope('summary'):
+        loss_summary_op = tf.summary.scalar(
+            'loss', critic.loss, collections=['loss'])
+
+        reward_ph = tf.placeholder(
+            shape=[None, ], name='reward', dtype=tf.float32)
+        target_q_values_ph = tf.placeholder(
+            shape=[None, 3], name='target_q_values', dtype=tf.float32)
+        y_t_ph = tf.placeholder(
+            shape=[None, 3], name='target_y_t', dtype=tf.float32)
+
+        tf.summary.scalar(
+            'reward', tf.reduce_mean(reward_ph), collections=['reward'])
+        tf.summary.scalar(
+            'target_q_values', tf.reduce_mean(target_q_values_ph),
+            collections=['reward'])
+        tf.summary.scalar(
+            'y_t', tf.reduce_mean(y_t_ph), collections=['reward'])
+
+        reward_summary_op = tf.summary.merge_all('reward')
 
     docker_client = docker.from_env()
 
@@ -53,12 +76,14 @@ def play_game(train_indicator=1):
     # Now load the weight
     try:
         print("Now we load the weight")
-        saver.restore(sess, '../weights/model.ckpt')
+        saver.restore(sess, checkpoint_file)
     except tf.errors.NotFoundError as e:
         print("{}: Weight not found".format(e))
 
     print("TORCS Experiment Start.")
     for i in range(episode_count):
+
+        recent_rewards = np.ones(50) * 1e9
 
         print("Episode : " + str(i) + " Replay Buffer " + str(buff.count()))
 
@@ -72,14 +97,15 @@ def play_game(train_indicator=1):
                          ob.rpm))
 
         total_reward = 0.
-        for _ in range(max_steps):
+
+        for j in range(max_steps):
             loss = 0
             epsilon -= 1.0 / explore
             a_t = np.zeros([1, action_dim])
             noise_t = np.zeros([1, action_dim])
 
             a_t_original = actor.predict(s_t.reshape(1, s_t.shape[0]))
-            
+
             noise_t[0][0] = train_indicator * \
                 max(epsilon, 0) * \
                 ou_func(a_t_original[0][0], 0.0, 0.60, 0.30)
@@ -96,9 +122,14 @@ def play_game(train_indicator=1):
 
             ob, r_t, done, _ = env.step(a_t[0])
 
-            s_t1 = np.hstack((ob.angle, ob.track, ob.trackPos, ob.speedX,
-                              ob.speedY, ob.speedZ, ob.wheelSpinVel / 100.0,
-                              ob.rpm))
+            recent_rewards[j % 50] = r_t
+
+            if np.median(recent_rewards) < 5.0:
+                break
+
+            s_t1 = np.hstack(
+                (ob.angle, ob.track, ob.trackPos, ob.speedX, ob.speedY,
+                 ob.speedZ, ob.wheelSpinVel / 100.0, ob.rpm))
 
             buff.add(s_t, a_t[0], r_t, s_t1, done)  # Add replay buffer
 
@@ -127,6 +158,20 @@ def play_game(train_indicator=1):
                 actor.train(states, grads)
                 actor.target_train()
                 critic.target_train()
+                if step % 500:
+                    loss_summary, reward_summary = sess.run(
+                            [loss_summary_op,
+                             reward_summary_op],
+                            feed_dict={
+                                critic.expected_critic: y_t,
+                                critic.state: states,
+                                critic.action: actions,
+                                reward_ph: rewards,
+                                target_q_values_ph: target_q_values,
+                                y_t_ph: y_t})
+
+                    train_writer.add_summary(loss_summary)
+                    train_writer.add_summary(reward_summary)
 
             total_reward += r_t
             s_t = s_t1
@@ -140,7 +185,7 @@ def play_game(train_indicator=1):
 
         if np.mod(i, 3) == 0:
             if (train_indicator):
-                saver.save(sess, '../weights/model.ckpt')
+                saver.save(sess, checkpoint_file)
 
         print("TOTAL REWARD @ " + str(i) +
               "-th Episode  : Reward " + str(total_reward))
@@ -148,8 +193,11 @@ def play_game(train_indicator=1):
         print("")
 
     env.end()  # This is for shutting down TORCS
+    train_writer.close()
     print("Finish.")
 
 
 if __name__ == "__main__":
     play_game()
+
+

src/gym_torcs_docker.py
@@ -36,8 +36,9 @@ class TorcsDockerEnv(object):
         self.observation_space = spaces.Box(low=low, high=high)
 
     def _start_docker(self):
-        os.system('nvidia-docker run' +
-		    ' --rm' + 
+        os.system(
+            'nvidia-docker run' +
+            ' --rm' +
             ' -it' +
             ' --volume="/tmp/.X11-unix/X0:/tmp/.X11-unix/X0:rw"' +
             ' --volume="/usr/lib/x86_64-linux-gnu/libXv.so.1:/usr/lib/x86_64-linux-gnu/libXv.so.1:rw"' +
@@ -83,7 +84,19 @@ class TorcsDockerEnv(object):
         action_torcs = self.client.R.d
         action_torcs['steer'] = this_action['steer']
         action_torcs['accel'] = this_action['accel']
+        action_torcs['brake'] = this_action['brake']
+
         action_torcs['gear'] = 1
+        if self.client.S.d['speedX'] > 50:
+            action_torcs['gear'] = 2
+        if self.client.S.d['speedX'] > 80:
+            action_torcs['gear'] = 3
+        if self.client.S.d['speedX'] > 110:
+            action_torcs['gear'] = 4
+        if self.client.S.d['speedX'] > 140:
+            action_torcs['gear'] = 5
+        if self.client.S.d['speedX'] > 170:
+            action_torcs['gear'] = 6
 
         # Save the privious full-obs from torcs for the reward calculation
         damage_pre = self.client.S.d["damage"]
@@ -99,25 +112,15 @@ class TorcsDockerEnv(object):
 
         # Reward setting Here
         # direction-dependent positive reward
-        track = np.array(obs['track'])
-        sp = np.array(obs['speedX'])
-        progress = sp * np.cos(obs['angle'])
+        progress = (
+            np.array(obs['speedX']) *
+            (np.cos(obs['angle']) - np.sin(obs['angle'])))
+
         reward = progress
 
         # collision detection
         if obs['damage'] - damage_pre > 0:
             reward = -1
-
-        # Termination judgement
-        # Episode is terminated if the car is out of track
-        if track.min() < 0:
-            reward = -1
-            self.client.R.d['meta'] = True
-
-        # Episode terminates if the progress of agent is small
-        if self.terminal_judge_start < self.time_step:
-            if progress < self.termination_limit_progress:
-                self.client.R.d['meta'] = True
 
         # Episode is terminated if the agent runs backward
         if np.cos(obs['angle']) < 0:
@@ -134,8 +137,8 @@ class TorcsDockerEnv(object):
         return self.observation
 
     def agent_to_torcs(self, u):
-        torcs_action = {'steer': u[0]}
-        torcs_action.update({'accel': u[1]})
+        torcs_action = {'steer': u[0], 'accel': u[1], 'brake': u[2]}
+
         return torcs_action
 
     def _make_observaton(self, raw_obs):

src/networks.py
@@ -16,6 +16,7 @@ class Network(object):
         self.action_size = action_size
         self.net_scope = net_scope
         self.target_net_scope = target_net_scope
+        self.is_training = False
 
         self.weights = None
         self.target_weights = None
@@ -27,6 +28,7 @@ class Network(object):
               for v1, v2 in zip(self.target_weights, self.weights)])
 
     def target_train(self):
+        self.is_training = True
         self.sess.run(self.cp_trgt_wgt_frm_wgt)
 
 
@@ -50,16 +52,18 @@ class CriticNetwork(Network):
         self.sess.run(tf.global_variables_initializer())
 
     def target_predict(self, states, actions):
+        self.is_training = False
         return self.sess.run(self.target_critic,
                              feed_dict={self.target_state: states,
                                         self.target_action: actions})
 
     def gradients(self, states, actions):
+        self.is_training = False
         return self.sess.run(self.action_grads, feed_dict={
             self.state: states, self.action: actions})[0]
 
     def train(self, expected_critic, states, actions):
-
+        self.is_training = True
         loss, _ = self.sess.run([self.loss, self.optimize], feed_dict={
             self.expected_critic: expected_critic, self.state: states,
             self.action: actions})
@@ -68,28 +72,42 @@ class CriticNetwork(Network):
 
     def _create_network(self, scope):
         with tf.variable_scope(scope):
-            state = tf.placeholder(shape=[None, self.state_size],
-                                   dtype=tf.float32, name='state')
-            action = tf.placeholder(shape=[None, self.action_size],
-                                    dtype=tf.float32, name='action')
 
-            s_layer1 = tf.layers.dense(inputs=state, name='s_layer_1',
-                                       units=CriticNetwork.HIDDEN1_UNITS,
-                                       activation=tf.nn.relu)
+            state = tf.placeholder(
+                shape=[None, self.state_size], dtype=tf.float32, name='state')
+            action = tf.placeholder(
+                shape=[None, self.action_size],
+                dtype=tf.float32, name='action')
 
-            s_layer2 = tf.layers.dense(inputs=s_layer1, name='s_layer_2',
-                                       units=CriticNetwork.HIDDEN2_UNITS)
+            s_layer1 = tf.layers.batch_normalization(
+                tf.layers.dense(
+                    inputs=state, activation=tf.nn.relu,
+                    units=CriticNetwork.HIDDEN1_UNITS),
+                training=self.is_training, name='s_layer_1')
 
-            a_layer = tf.layers.dense(inputs=action, name='a_layer',
-                                      units=CriticNetwork.HIDDEN2_UNITS,
-                                      activation=tf.nn.relu)
+            s_layer2 = tf.layers.batch_normalization(
+                tf.layers.dense(
+                    inputs=s_layer1,
+                    units=CriticNetwork.HIDDEN2_UNITS),
+                training=self.is_training, name='s_layer_2')
 
-            c_layer = tf.layers.dense(inputs=(s_layer2 + a_layer),
-                                      name='c_layer', activation=tf.nn.relu,
-                                      units=CriticNetwork.HIDDEN2_UNITS)
+            a_layer = tf.layers.batch_normalization(
+                tf.layers.dense(
+                    inputs=action,
+                    units=CriticNetwork.HIDDEN2_UNITS),
+                training=self.is_training, name='a_layer')
 
-            critic = tf.layers.dense(inputs=c_layer, name='critic',
-                                     units=self.action_size)
+            c_layer = tf.layers.batch_normalization(
+                tf.layers.dense(
+                    inputs=(s_layer2 + a_layer),
+                    activation=tf.nn.relu,
+                    units=CriticNetwork.HIDDEN2_UNITS),
+                training=self.is_training, name='c_layer')
+
+            critic = tf.layers.batch_normalization(
+                tf.layers.dense(inputs=c_layer,
+                                units=self.action_size),
+                training=self.is_training, name='critic')
 
             weights = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES,
                                         scope=scope)
@@ -127,13 +145,16 @@ class ActorNetwork(Network):
         self.sess.run(tf.global_variables_initializer())
 
     def predict(self, states):
+        self.is_training = False
         return self.sess.run(self.action, feed_dict={self.state: states})
 
     def target_predict(self, states):
+        self.is_training = False
         return self.sess.run(self.target_action,
                              feed_dict={self.target_state: states})
 
     def train(self, states, action_grads):
+        self.training = True
         self.sess.run(self.optimize, feed_dict={
             self.state: states, self.action_gradient: action_grads})
 
@@ -141,22 +162,38 @@ class ActorNetwork(Network):
         with tf.variable_scope(scope):
             state = tf.placeholder(tf.float32, [None, self.state_size],
                                    name='state')
-            hidden0 = tf.layers.dense(inputs=state, name='hidden_0',
-                                      units=ActorNetwork.HIDDEN1_UNITS,
-                                      activation=tf.nn.relu)
-            hidden1 = tf.layers.dense(inputs=hidden0, name='hidden_1',
-                                      units=ActorNetwork.HIDDEN2_UNITS,
-                                      activation=tf.nn.relu)
-            steering = tf.layers.dense(inputs=hidden1, name='steering',
-                                       units=1, activation=tf.nn.tanh)
-            acceleration = tf.layers.dense(inputs=hidden1, name='acceleration',
-                                           units=1, activation=tf.nn.sigmoid)
-            brake = tf.layers.dense(inputs=hidden1, name='brake',
-                                    units=1, activation=tf.nn.sigmoid)
-            action = tf.concat([steering, acceleration, brake], name='action',
-                               axis=1)
-            weights = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES,
-                                        scope=scope)
+
+            hidden0 = tf.layers.batch_normalization(
+                tf.layers.dense(
+                    inputs=state, activation=tf.nn.relu,
+                    units=ActorNetwork.HIDDEN1_UNITS),
+                training=self.is_training, name='hidden_0')
+
+            hidden1 = tf.layers.batch_normalization(
+                tf.layers.dense(inputs=hidden0, activation=tf.nn.relu,
+                                units=ActorNetwork.HIDDEN2_UNITS),
+                training=self.is_training, name='hidden_1')
+
+            steering = tf.layers.batch_normalization(
+                tf.layers.dense(
+                    inputs=hidden1, units=1, activation=tf.nn.tanh),
+                training=self.is_training, name='steering')
+
+            acceleration = tf.layers.batch_normalization(
+                tf.layers.dense(
+                    inputs=hidden1, units=1, activation=tf.nn.sigmoid),
+                training=self.is_training, name='acceleration')
+
+            brake = tf.layers.batch_normalization(
+                tf.layers.dense(
+                    inputs=hidden1, units=1, activation=tf.nn.sigmoid),
+                training=self.is_training, name='brake')
+
+            action = tf.concat(
+                [steering, acceleration, brake], name='action', axis=1)
+
+            weights = tf.get_collection(
+                tf.GraphKeys.TRAINABLE_VARIABLES, scope=scope)
 
         return action, weights, state
 
@@ -168,3 +205,6 @@ class ActorNetwork(Network):
         optimize = tf.train.AdamOptimizer(
             self.learning_rate).apply_gradients(grads)
         return optimize, action_gradient
+
+
+